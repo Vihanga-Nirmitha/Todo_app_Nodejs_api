ce37f140241b171e36e0713fe45ea2133bc54595
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2023-12-20 08:10:38 +0530

    Create connection pool

--- src/controller/task.http.controller.ts
+++ src/controller/task.http.controller.ts
@@ -8,6 +8,15 @@ controller.post('/',saveTask)
 controller.patch('/:id',updateTask)
 controller.delete('/:id',deleteTask)
 
+const pool = mysql.createPool({
+    database: process.env.DB_NAME,
+    port: +(process.env.DB_PORT ?? 3306),
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    connectionLimit: +process.env.DB_CONNECTION_LIMIT!
+});
+
 function getAlltasks(req:Request, res: Response){
     if(!req.query.email) res.sendStatus(400);
     res.send('<h1>Customer controller: GET</h1>')
